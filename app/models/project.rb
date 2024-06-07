a49a8aeaeb77a2bba4d37494989923f8cccf946e
Brian Baggs <brian@carbidesecure.com>
2024-06-07 10:12:41 -0300

    Updating projects model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,9 @@
 class Project < ApplicationRecord
+    after_initialize :set_defaults
+    scope :almost_complete, -> { where('percent_complete > 85.0') }
+    scope :still_needs_work, -> { where('percent_complete < 25.0') }
+
+    def set_defaults
+        self.percent_complete ||= 0.0
+    end    
 end
