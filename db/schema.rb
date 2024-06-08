d3381d926536bc3ee5ea61c17849ad91fb608486
Brian Baggs <brian@carbidesecure.com>
2024-06-08 13:59:04 -0300

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_08_152333) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_08_161525) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.decimal "percent_complete"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "stage"
   end
 
   create_table "tasks", force: :cascade do |t|
